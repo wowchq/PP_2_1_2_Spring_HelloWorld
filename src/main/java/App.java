9f9ac7d1a8b530571bd45caf566e3043f132341d
wowchq <vova17082002@icloud.com>
2024-11-11 15:26:29 +0300

    prototype bean cat is  implemented

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld myBean = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение ссылок бинов HelloWorld: bean = myBean? " + bean.equals(myBean)
+                + "\nСравнение ссылок бинов Cat: cat = cat2? " + cat.equals(cat2));
     }
 }
